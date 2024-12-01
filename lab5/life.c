cbe56ada03e735936e385773e0c3769e9d6a0e60
Junyi Zhu <louis.zhu@mail.utoronto.ca>
2024-12-01 12:34:20 -0500

    trying openMp

--- lab5/life.c
+++ lab5/life.c
@@ -4,6 +4,7 @@
  ****************************************************************************/
 #include "life.h"
 #include "util.h"
+#include <omp.h>
 
 /*****************************************************************************
  * Helper function definitions
@@ -29,32 +30,37 @@ game_of_life (char* outboard,
 	      const int ncols,
 	      const int gens_max)
 {
-    const int LDA = nrows;  // Leading dimension of the array
+    const int LDA = nrows; // Leading dimension of the array
+    const int TILE_SIZE = 32; // Tile size (minimum board size)
+
     int curgen;
 
     for (curgen = 0; curgen < gens_max; curgen++) {
-        for (int i = 0; i < nrows; i++) {
-            for (int j = 0; j < ncols; j += 16) { // Process 16 cells at a time
-                // Compute neighbors for each group of 16 cells
-                for (int k = 0; k < 16; k++) {
-                    const int col = j + k; // Column offset for current group
-                    const int inorth = mod(i - 1, nrows);
-                    const int isouth = mod(i + 1, nrows);
-                    const int jwest = mod(col - 1, ncols);
-                    const int jeast = mod(col + 1, ncols);
+        #pragma omp parallel for collapse(2) schedule(static)
+        for (int ti = 0; ti < nrows; ti += TILE_SIZE) {
+            for (int tj = 0; tj < ncols; tj += TILE_SIZE) {
+                // Process each tile
+                for (int i = ti; i < ti + TILE_SIZE && i < nrows; i++) {
+                    for (int j = tj; j < tj + TILE_SIZE && j < ncols; j++) {
+                        // Calculate neighbors with toroidal wrapping
+                        const int inorth = mod(i - 1, nrows);
+                        const int isouth = mod(i + 1, nrows);
+                        const int jwest = mod(j - 1, ncols);
+                        const int jeast = mod(j + 1, ncols);
 
-                    const char neighbor_count =
-                        BOARD(inboard, inorth, jwest) +
-                        BOARD(inboard, inorth, col) +
-                        BOARD(inboard, inorth, jeast) +
-                        BOARD(inboard, i, jwest) +
-                        BOARD(inboard, i, jeast) +
-                        BOARD(inboard, isouth, jwest) +
-                        BOARD(inboard, isouth, col) +
-                        BOARD(inboard, isouth, jeast);
+                        const char neighbor_count =
+                            BOARD(inboard, inorth, jwest) +
+                            BOARD(inboard, inorth, j) +
+                            BOARD(inboard, inorth, jeast) +
+                            BOARD(inboard, i, jwest) +
+                            BOARD(inboard, i, jeast) +
+                            BOARD(inboard, isouth, jwest) +
+                            BOARD(inboard, isouth, j) +
+                            BOARD(inboard, isouth, jeast);
 
-                    // Update the output board
-                    BOARD(outboard, i, col) = alivep(neighbor_count, BOARD(inboard, i, col));
+                        // Update the state in the output board
+                        BOARD(outboard, i, j) = alivep(neighbor_count, BOARD(inboard, i, j));
+                    }
                 }
             }
         }
