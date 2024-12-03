e7ea666884e4b87549321347b1dd2d3fe3819685
Junyi Zhu <louis.zhu@mail.utoronto.ca>
2024-12-03 10:36:33 -0500

    Added threading to main loop

--- lab5/life.c
+++ lab5/life.c
@@ -3,8 +3,7 @@
  * Parallelized and optimized implementation of the game of life resides here
  ****************************************************************************/
 #include "life.h"
-#include "util.h"
-//#include <omp.h>
+#include <pthread.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,12 +13,12 @@
  * Helper function definitions
  ****************************************************************************/
 #define BOARD( __board, __i, __j )  (__board[(__j) + LDA*(__i)])
+#define NUMTHREADS 4
 /*****************************************************************************
  * Game of life implementation
  ****************************************************************************/
 
 typedef struct row{
-    int Y;
     struct row* prev;
     struct row* next;
     int* x_tail;
@@ -29,6 +28,152 @@ typedef struct row{
 typedef enum {
 		DEAD, LIVE
 } state;
+state stateList[1 << 9]; //global state table
+
+// Thread argument
+typedef struct {
+  int thread_id;
+  row* rowIn;
+  row* rowOut;
+  int ncols;
+  int nrows;
+} thread_arg_t;
+
+void* processNextRows(void* arg){
+    int bitmap;
+    int startRow, rowPerthread;
+    int *prev, *this, *next, *x_tail_this, *x_tail_prev, *x_tail_next;
+    int *out;
+    int x;
+
+    int nrows = ((thread_arg_t*)arg)->nrows;
+    int ncols = ((thread_arg_t*)arg)->ncols;
+    int threadId = ((thread_arg_t*)arg)->thread_id;
+    row* rowIn = ((thread_arg_t*)arg)->rowIn;
+    row* rowOut = ((thread_arg_t*)arg)->rowOut;
+    rowPerthread = nrows / NUMTHREADS;
+    startRow = threadId * rowPerthread;
+
+    for (int row = startRow; row < startRow + rowPerthread; row++){
+        // printf("row %d: \n", row);
+        bitmap = 0;
+        //set up x and x_poiters
+        this = rowIn[row].x_cords;
+        prev = rowIn[row].prev->x_cords;
+        next = rowIn[row].next->x_cords;
+        out = rowOut[row].x_cords;
+        x = 50000;
+        *out = 50000;
+        rowOut[row].x_tail = out;
+        x = *prev;
+        //find the left most cell
+		if(x > *this)
+			x = *this;
+		if(x > *next)
+			x = *next;
+        if (x == 50000) break; //scan line empty
+        
+        //fecth tail x cords for the current scan line (largest x in scan line)
+        x_tail_this = rowIn[row].x_tail;
+        x_tail_next = rowIn[row].next->x_tail;
+        x_tail_prev = rowIn[row].prev->x_tail;
+
+        //check for wrap and handle x = 0 properly by initializing bitmap with wrapped column
+        //insert column at x = 0 into bitmap[3-5]
+        if(*prev == 0) {
+			bitmap |= 010;
+			prev++;
+		}
+		if(*this == 0) {
+			bitmap |= 020;
+			this++;
+		}
+		if(*next == 0) {
+			bitmap |= 040;
+			next++;
+		}
+        //insert column at x = ncols - 1 into bitmap[0-2]
+        if(*x_tail_prev == ncols - 1) {
+			bitmap |= 01;
+		}
+		if(*x_tail_this == ncols - 1) {
+			bitmap |= 02;
+		}
+		if(*x_tail_next == ncols - 1) {
+			bitmap |= 04;
+		}
+        // printf("    head wrap bitmap @ x = %d is: %d\n", x, bitmap);
+        if (bitmap != 0) x = 1;
+
+        //Enter main loop to scan along x cords
+        for(;;) {
+			for(;;) {
+				//insert column at x >= 1 into bitmap[6-8]
+				if(*prev == x) {
+					bitmap |= 0100;
+					prev++;
+				}
+				if(*this == x) {
+					bitmap |= 0200;
+					this++;
+				}
+				if(*next == x) {
+					bitmap |= 0400;
+					next++;
+				}
+				/* what does this bitmap indicate? */
+                // printf("    bitmap @ x = %d is: %d\n", x, bitmap);
+				if(stateList[bitmap] == LIVE)
+					*out++ = x - 1;
+				else if(bitmap == 000)
+					break;
+				/* move right */
+				bitmap >>= 3;
+				x += 1;
+                //just written the cell at ncol - 2
+                if (x >= ncols) break;
+			}
+            x = *prev;
+            //find the left most cell
+		    if(x > *this)
+		    	x = *this;
+		    if(x > *next)
+		    	x = *next;
+            if (x == 50000){
+                //handle wrapping at x = ncol -1
+                // printf("    tail wrap entry bitmap @ x = %d is: %d\n", x, bitmap);
+                //insert column at x = 0 into bitmap[6-8]
+                if(*(rowIn[row].prev->x_cords) == 0) {
+		        	bitmap |= 0100;
+		        }
+		        if(*(rowIn[row].x_cords) == 0) {
+		        	bitmap |= 0200;
+		        }
+		        if(*(rowIn[row].next->x_cords) == 0) {
+		        	bitmap |= 0400;
+		        }
+                // printf("    tail wrap bitmap @ x = %d is: %d\n", x, bitmap);
+                if(stateList[bitmap] == LIVE){
+                    *out++ = ncols - 1;
+                }
+                break; //entire scan line is empty
+            }
+		}
+        rowOut[row].x_tail = --out;
+        out++;
+        //terminate xcord array
+        *out++ = 50000;
+        *out++ = 50000;
+        *out++ = 50000;
+        *out++ = 50000;
+        *out++ = 50000;
+        for (int j = 0; rowOut[row].x_cords[j] != 50000; j++){
+            // printf("%d ", rowOut[row].x_cords[j]);
+        }
+        // printf("\n");
+    }
+    pthread_exit(NULL);
+}
 
 char*
 game_of_life (char* outboard, 
@@ -42,13 +187,10 @@ game_of_life (char* outboard,
     row *rowIn = (row*) malloc(10000 * sizeof(*rowIn));
     row *rowOut = (row*) malloc(10000 * sizeof(*rowOut));
     row* temp;
-    state stateList[1 << 9];
     int x_cords_idx;
     int liveCnt, bmpIdx;
-    int x;
-    int *prev, *this, *next, *x_tail_this, *x_tail_prev, *x_tail_next;
-    int *out;
-    int bitmap = 0;
+    pthread_t threads[4];
+    thread_arg_t thread_args[4];
 
     //init bitmap arr
     for(int bitmap_state_idx = 0; bitmap_state_idx < 1<<9; bitmap_state_idx++) {
@@ -108,131 +250,27 @@ game_of_life (char* outboard,
     rowOut[0].prev = &rowOut[nrows - 1];
     rowOut[nrows - 1].next = &rowOut[0];
 
+    //Main iteration processing loop
     for (int it = 0; it < gens_max; it++){
         //for all rows
-        for (int row = 0; row < nrows; row++){
-            // printf("row %d: \n", row);
-            bitmap = 0;
-            //set up x and x_poiters
-            this = rowIn[row].x_cords;
-            prev = rowIn[row].prev->x_cords;
-            next = rowIn[row].next->x_cords;
-            out = rowOut[row].x_cords;
-            x = 50000;
-            *out = 50000;
-            rowOut[row].x_tail = out;
-            x = *prev;
-            //find the left most cell
-	    	if(x > *this)
-	    		x = *this;
-	    	if(x > *next)
-	    		x = *next;
-            if (x == 50000) break; //scan line empty
-            
-            //fecth tail x cords for the current scan line (largest x in scan line)
-            x_tail_this = rowIn[row].x_tail;
-            x_tail_next = rowIn[row].next->x_tail;
-            x_tail_prev = rowIn[row].prev->x_tail;
-
-            //check for wrap and handle x = 0 properly by initializing bitmap with wrapped column
-            //insert column at x = 0 into bitmap[3-5]
-            if(*prev == 0) {
-	    		bitmap |= 010;
-	    		prev++;
-	    	}
-	    	if(*this == 0) {
-	    		bitmap |= 020;
-	    		this++;
-	    	}
-	    	if(*next == 0) {
-	    		bitmap |= 040;
-	    		next++;
-	    	}
-            //insert column at x = ncols - 1 into bitmap[0-2]
-            if(*x_tail_prev == ncols - 1) {
-	    		bitmap |= 01;
-	    	}
-	    	if(*x_tail_this == ncols - 1) {
-	    		bitmap |= 02;
-	    	}
-	    	if(*x_tail_next == ncols - 1) {
-	    		bitmap |= 04;
-	    	}
-            // printf("    head wrap bitmap @ x = %d is: %d\n", x, bitmap);
-            if (bitmap != 0) x = 1;
-
-            //Enter main loop to scan along x cords
-            for(;;) {
-	    		for(;;) {
-	    			//insert column at x >= 1 into bitmap[6-8]
-	    			if(*prev == x) {
-	    				bitmap |= 0100;
-	    				prev++;
-	    			}
-	    			if(*this == x) {
-	    				bitmap |= 0200;
-	    				this++;
-	    			}
-	    			if(*next == x) {
-	    				bitmap |= 0400;
-	    				next++;
-	    			}
-	    			/* what does this bitmap indicate? */
-                    // printf("    bitmap @ x = %d is: %d\n", x, bitmap);
-	    			if(stateList[bitmap] == LIVE)
-	    				*out++ = x - 1;
-	    			else if(bitmap == 000)
-	    				break;
-	    			/* move right */
-	    			bitmap >>= 3;
-	    			x += 1;
-                    //just written the cell at ncol - 2
-                    if (x >= ncols) break;
-	    		}
-                x = *prev;
-                //find the left most cell
-	    	    if(x > *this)
-	    	    	x = *this;
-	    	    if(x > *next)
-	    	    	x = *next;
-                if (x == 50000){
-                    //handle wrapping at x = ncol -1
-                    // printf("    tail wrap entry bitmap @ x = %d is: %d\n", x, bitmap);
-                    //insert column at x = 0 into bitmap[6-8]
-                    if(*(rowIn[row].prev->x_cords) == 0) {
-	    	        	bitmap |= 0100;
-	    	        }
-	    	        if(*(rowIn[row].x_cords) == 0) {
-	    	        	bitmap |= 0200;
-	    	        }
-	    	        if(*(rowIn[row].next->x_cords) == 0) {
-	    	        	bitmap |= 0400;
-	    	        }
-                    // printf("    tail wrap bitmap @ x = %d is: %d\n", x, bitmap);
-                    if(stateList[bitmap] == LIVE){
-                        *out++ = ncols - 1;
-                    }
-                    break; //entire scan line is empty
-                }
-	    	}
-            rowOut[row].x_tail = --out;
-            out++;
-            //terminate xcord array
-            *out++ = 50000;
-            *out++ = 50000;
-            *out++ = 50000;
-            *out++ = 50000;
-            *out++ = 50000;
-            for (int j = 0; rowOut[row].x_cords[j] != 50000; j++){
-                // printf("%d ", rowOut[row].x_cords[j]);
-            }
-            // printf("\n");
+        for (int threadId = 0; threadId < NUMTHREADS; threadId++){
+            thread_args[threadId].thread_id = threadId;
+            thread_args[threadId].ncols = ncols;
+            thread_args[threadId].nrows = nrows;
+            thread_args[threadId].rowIn = rowIn;
+            thread_args[threadId].rowOut = rowOut;
+            pthread_create(&threads[threadId], NULL, processNextRows, &thread_args[threadId]);
+        }
+        // Wait for threads to complete
+        for (int i = 0; i < NUMTHREADS; i++) {
+          pthread_join(threads[i], NULL);
         }
         temp = rowIn;
         rowIn = rowOut;
         rowOut = temp;
     }
 
+    //Construct output
     memset(outboard, 0, sizeof(char) * ncols * nrows * 2);
     for (int i = 0; i < nrows; i++){
         for (int j = 0; rowIn[i].x_cords[j] != 50000; j++){
